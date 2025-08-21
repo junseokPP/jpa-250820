1edd552ee6dc3731de54c1a1a2c651a1b6c4cc79
junseokPP <pjs981130@gmail.com>
2025-08-21 11:15:59 +0900

    14강 : final의 조합으로 특정필드만 초기화

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -2,26 +2,27 @@ package com.back.domain.post.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@RequiredArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String title;
+    private final String title;
 
     @Column(columnDefinition = "TEXT")
-    private String content;
+    private final String content;
 
-    public Post() {}
-
-    public Post(String title, String content) {
-        this.title = title;
-        this.content = content;
+    public Post(){
+        this.title = "";
+        this.content = "";
     }
+
 }
