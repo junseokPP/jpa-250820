94ac69f83b452f3b1bfbc39c4ef8f310fd576b7c
junseokPP <pjs981130@gmail.com>
2025-09-01 10:47:27 +0900

    36강 : PostRepositoryTest 도입

---
+++ src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.back.domain.post.repository;
+
+import com.back.domain.post.entity.Post;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    private  PostRepository postRepository;
+
+    @Test
+    @DisplayName("2번 글 조회")
+    void t1(){
+        Post post1 = postRepository.findById(2).get();
+
+        assertThat(post1.getId()).isEqualTo(2);
+        assertThat(post1.getTitle()).isEqualTo("제목2");
+        assertThat(post1.getContent()).isEqualTo("내용2");
+    }
+}
